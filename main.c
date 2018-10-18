95db49131b2ff77dd2ad507ab966edc3f1fc66d4
BrenoFroes <breno_alves@msn.com>
2018-10-18 17:24:26 -0300

    Pronto

--- main.c
+++ main.c
@@ -7,158 +7,163 @@ typedef struct no {
     struct no *prox;
 } No;
 
-/* Cria uma lista vazia */
-No *criaLista() {
-    No *novo = (No *)malloc(sizeof(No));
+No *criaLista(){
+    No *novo = (No *) malloc(sizeof (No));
+    novo->prox = NULL;
     novo->dado = 0;
-    novo->prox = novo;
     return novo;
 }
 
-/* Verifica se a lista é vazia */
-int vazia(No **l) {
-    if ((*l)->dado == 0)
-        return 1;
-    else
-        return 0;
+int vazia(No *tabela[40], int chave) {
+    if (tabela[chave]->dado == 0 || tabela[chave]->dado == -1 ) return 1;
+    else return 0;
 }
 
-/* Verifica se a tabela está cheia */
-int cheia(No *tabela[40]){
-    int flag = 1; // 1 representa tabela cheia
-    for(int i = 0; i<40; i++){
-        if(tabela[i]->dado == 0){
+int cheiaTabela(No *tabela[40]){
+    int flag = 1;
+    for (int i=0; i<40; i++){
+        if (tabela[i]->dado == 0 || tabela[i]->dado == -1) {
             flag = 0;
-            return flag;
         }
     }
     return flag;
 }
 
-/* Insere nó com colisão */
-void colisao(No **tabela, No *n){
-    if(cheia(tabela) == 0){
-        for(int i = 40; i>=0; i--){
-            if(tabela[i]->dado == 0){
-                tabela[i]->prox = n;
-                tabela[i]->dado++;
-                break;
+int vaziaTabela(No *tabela[40]){
+    int flag = 1;
+    for (int i=0; i<40; i++){
+        if(tabela[i]->dado  !=  0 && tabela[i]->dado != -1){
+            flag = 0;
+        }
+    }
+    return flag;
+}
+
+void colisao (No *tabela[40], int chave, No *n){
+    No *aux = tabela[chave];
+    while(aux->prox != NULL){
+        if (aux->prox->dado == 0 || aux->prox->dado == 1){
+            aux->prox = n;
+            if(aux->prox->prox != NULL){
+                n->prox = aux->prox->prox;
             }
         }
-    }else{
-        printf("Overflow");
-        exit(0);
+        aux = aux->prox;
+    }
+    if(aux->prox == NULL){
+        aux->prox = n;
+    }
+    if(cheiaTabela(tabela) == 0){
+        for(int i = 40; i>0; i--){
+            if (tabela[i]->dado == 0 || tabela[i]->dado == -1){
+                tabela[i] = n;
+                return;
+            }
+        }
+    }
+    else {
+        printf("____________________ \n");
+        printf ("OVERFLOW \n\n");
+        return;
     }
 }
 
-/* Insere novo nó na lista */
-void insereFim(No **tabela, No **l, int valor) {
-    No *novo = (No *)malloc(sizeof(No));
+void inserir (No *tabela[40], int valor) {
+    No *novo = (No *) malloc (sizeof (No));
     novo->dado = valor;
-    if (vazia(l) == 1) {
-        (*l)->prox = novo;
-        novo->prox = *l;
-        (*l)->dado++;
-    } else if ((*l)->dado == 1) {
-        (*l)->prox->prox = novo;
-        novo->prox = *l;
-        (*l)->dado++;
-        colisao(tabela, novo);
-    } else {
-        No *aux = *l;
-        do {
-            aux = aux->prox;
-        } while (aux->prox != *l);
-        aux->prox = novo;
-        novo->prox = *l;
-        (*l)->dado++;
-        colisao(tabela, novo);
-    }
-}
-
-/* Remove nó da lista */
-void remover(No **l, int valor){
-    No *aux = (*l)->prox;
-    if(vazia(l) == 1){
-        printf("Lista vazia\n");
-    }else if((*l)->dado == 1){
-
-        free(aux);
-        (*l)->dado--;
-    }else{
-        No *aux = (*l)->prox;
-        No *aux2 = *l;
-        while(aux->prox != NULL){
-            if(aux->dado == valor) break;
-            aux2 = aux;
-            aux = aux->prox;
-        }
-        if(aux->prox == NULL){
-            aux2->prox = NULL;
-        }else{
-            aux2->prox = aux->prox;
-        }
-        free(aux);
-        (*l)->dado--;
-    }
-}
-
-/* Imprime a tabela */
-void imprime(No *tabela[40]) {
-    for(int i = 0; i<40; i++){
-        if(i < 5){
-            printf("%d - %d\n", i, tabela[i]->prox->dado);
-            printf("________\n");// MF //
+    novo->prox = NULL;
+    int chave = valor%5;
+    if (vazia(tabela, chave)  ==  0){
+        if (cheiaTabela(tabela) == 1) {
+            printf("____________________ \n");
+            printf ("OVERFLOW\n\n");
+            exit(0);
         }
         else{
-            printf("%d\n", tabela[i]->prox->dado);
-            printf("_____\n"); // MF //
+            colisao(tabela, chave, novo);
         }
+    }
+    else{
+        tabela[chave] = novo;
 
     }
 }
-
-/* Insere um novo numero na tabela */
-void insereNumero (No *tabela[40], int n){
-    int chave = n%5;
-    insereFim(tabela, &tabela[chave], n);
+int busca(No *tabela[40], int valor){
+    int flag = 0;
+    for(int i =0; i<40;i++){
+        if(tabela[i]->dado == valor)
+            flag = 1;
+    }
+    return flag;
 }
 
-/* Remove um numero da tabela */
-void removeNumero(No *tabela[40], int n){
-    int chave = n%5;
-    remover(&tabela[chave], n);
+void remover (No *tabela[40], int valor){
+    int flag = 1;
+    if (vaziaTabela(tabela) == 1){
+        printf("____________________ \n");
+        printf("UNDERFLOW \n\n");
+    }
+    else{
+        if(busca(tabela, valor) == 0){
+            printf("____________________ \n");
+            printf("VALOR NÃO ENCONTRADO \n\n");
+        }
+        else{
+            int chave = valor%5;
+            No *aux = tabela[chave]->prox;
+            No *aux2 = tabela[chave];
+            if (aux2->dado == valor){
+                aux2->dado = -1;
+            }
+            else{
+                while (aux->prox != NULL){
+                    if(aux->dado == valor){
+                        aux->dado = -1;
+                        flag = 0;
+                    }
+                    aux2 = aux;
+                    aux = aux->prox;
+                }
+            }
+        }
+    }
 }
+
+void imprimir (No *tabela[40]){
+    printf("LISTA INTERIOR HOMOGÊNEA \n");
+    printf("________________________ \n\n");
+    for(int i = 0; i < 40; i++){
+        if(tabela[i]->dado == 0) printf ("[%d]-> CASA VAZIA\n", i);
+        if(tabela[i]->dado == -1) printf ("[%d]-> CASA LIBERADA\n", i);
+        else if(tabela[i]->dado != 0)printf ("[%d]-> %d\n", i, tabela[i]->dado);
+    }
+}
+void imprimirLista(No *l){
+    printf("\nLISTA DA CASA %d\n", l->dado%5);
+    printf("__________________\n \n");
+    No *aux = l;
+    int cont = 0;
+    while(aux != NULL){
+        printf("aux->dado: %d\n", aux->dado);
+        aux = aux->prox;
+        cont++;
+    }
+    printf("Quantidade de itens nessa lista: %d\n", cont);
+}
+
 
 int main(void) {
-    No* tabela [40] ;
-    int x;
+    No *tabela [40];
     for(int i = 0; i<40; i++){
         tabela[i] = criaLista();
     }
-    /* srand(time(NULL)); /* essa linha permite que a cada "run" sejam gerados números diferentes, caso queira os mesmos aleatórios sempre só deletá-la */
-    for (int i=0; i<40; i++){
-        insereNumero(tabela, rand ()%100);
+    srand(time(NULL));
+    for (int i = 0; i<40;i++){
+        inserir(tabela, rand() %100);
     }
-    /*insereNumero(tabela, 7);
-    insereNumero(tabela, 14);
-    insereNumero(tabela, 42);
-    insereNumero(tabela, 32);
-    insereNumero(tabela, 4);
-    insereNumero(tabela, 9);
-    insereNumero(tabela, 22);
-    insereNumero(tabela, 12);
-    insereNumero(tabela, 32);
-    insereNumero(tabela, 32);
-    insereNumero(tabela, 32);*/
-
-
-    imprime(tabela);
-    removeNumero(tabela, 11);
-    imprime(tabela);
+    imprimir(tabela);
+    for (int i = 0; i<5; i++){
+        imprimirLista(tabela[i]);
+    }
     return 0;
-
 }
-
-// modificado = MF //
-// OBS: TODOS OS VALORES IGUAIS A 10 FORAM SUBSTITUÍDOS POR 40 //
